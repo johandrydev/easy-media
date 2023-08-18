71fc59915cf3d87ae8463d3c70cac68077290d19
Johandry Mora <johandrydev@gmail.com>
2023-08-17 21:34:34 -0500

    Initial commit

---
+++ back/src/app.ts
@@ -0,0 +1,29 @@
+import express, { NextFunction, Request, Response, json } from "express";
+import cors from "cors";
+
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(json());
+// Routes
+
+// Basic error handler
+const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  switch (err.name) {
+    case 'ValidationError':
+      res.status(400);
+      break;
+  
+    default:
+      res.status(500).send({ message: 'Internal Server Error' });
+      return;
+  }
+
+	return res.json({
+		message: err.message
+	})
+}
+app.use(errorHandler);
+app.listen(port, () => console.log(`Server is running on port ${port}`));
